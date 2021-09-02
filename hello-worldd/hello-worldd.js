fdf4c3c8a65aaba761aa03a668ff42411fc0729d
Nick De Sanno <nickdesanno@Nicks-MacBook-Pro.local>
2021-09-02 14:33:37 -0400

    add existing file

---
+++ hello-worldd/hello-worldd.js
@@ -0,0 +1,3 @@
+import { HelloWorldd } from './src/HelloWorldd.js';
+
+window.customElements.define('hello-worldd', HelloWorldd);
---
+++ hello-worldd/index.js
@@ -0,0 +1,1 @@
+export { HelloWorldd } from './src/HelloWorldd.js';
